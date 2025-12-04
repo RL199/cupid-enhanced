23bd7cf10f8fb927525e836d20cbc5697d223048
Roy Levi <rrooyy199@gmail.com>
2025-12-04 19:13:13 +0200

    feat: enhance premium features injection for improved user experience

--- scripts/api-interceptor.js
+++ scripts/api-interceptor.js
@@ -55,16 +55,32 @@
                     data.data.me.premiums = {};
                 }
                 // Enable all premium features to bypass paywalls
-                data.data.me.premiums.VIEW_VOTES = true;
-                data.data.me.premiums.INTROS = true;
-                data.data.me.premiums.HELICOPTER = true;
-                data.data.me.premiums.ALIST_PREMIUM = true;
-                data.data.me.premiums.INC_LIKES = true;
-                data.data.me.premiums.BOOST = true;
-                data.data.me.premiums.ADFREE = true;
-                data.data.me.premiums.INCOGNITO = true;
-                data.data.me.premiums.READ_RECEIPTS = true;
-                data.data.me.premiums.A_LIST = true;
+                data.data.me.premiums.VIEW_VOTES = true;  // see who was interested in you
+                data.data.me.premiums.ALIST_PREMIUM = true; // Display premium badge
+                data.data.me.premiums.ALIST_PREMIUM_PLUS = true; // Display premium plus badge
+
+                // Premium features found in module 88074
+                data.data.me.premiums.intoyou = true; // See who likes you
+                data.data.me.premiums.comfree = true; // Ad-free
+                data.data.me.premiums.unlimited_likes = true;
+                data.data.me.premiums.intros = true;
+                data.data.me.premiums.dealbreakers = true;
+                data.data.me.premiums.see_more_people = true;
+                data.data.me.premiums.questions = true;
+                data.data.me.premiums.superlikes = true;
+                data.data.me.premiums.rewind = true;
+                data.data.me.premiums.question_search = true;
+                data.data.me.premiums.who_likes_you = true;
+                data.data.me.premiums.question_answers = true;
+                data.data.me.premiums.likes_list_sort = true;
+                data.data.me.premiums.priority_likes = true;
+                data.data.me.premiums.read_receipts = true;
+                data.data.me.premiums.boost = true;
+                data.data.me.premiums.incognito = true;
+
+                // Additional flags
+                data.data.me.hasIncognito = true;
+
                 modified = true;
             }
 
